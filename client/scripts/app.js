5ca5e6b66dfc88d285658e642fe01779785b727c
Zach Gibb <zachgibb@gmail.com>
2014-08-04 23:40:30 -0400

    end of day 1

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@
 // roomname: ""
 // text: "test"
 // username: "ken"
-
+var lastMessageId = '';
 var app = {
   init: function(){},
   send: function(){
@@ -14,8 +14,11 @@ var app = {
   fetch: function(){
     $.ajax({
       url: 'https://api.parse.com/1/classes/chatterbox',
+      data: {
+        order: "-createdAt",
+      },
       type: "GET",
-      contentType: 'application/json',
+      contentType: 'application/json; charset=utf-8',
       success: function (data) {
         console.log('success');
         makeMessage(data.results);
@@ -24,20 +27,53 @@ var app = {
         console.log('failure');
       }
     });
-    
   }
 };
 
 var makeMessage = function(array) {
+  // $('.messages').empty();
   var start = "<div class=message>";
   var end = "</div>";
-  for (var i = 0; i < array.length; i++) {
-    var current = array[i];
-    console.log('---------------------------------');
-    var roomname = '<span class="roomname">' + current.roomname + '</span>';
-    var username = '<span class="username">' + current.username + '</span>';
-    var text = '<span class="text">' + current.text + '</span>';
+  if ( !lastMessageId ){
+    for (var i = 0; i < array.length; i++) {
+      var current = array[i];
+      var roomname = '<span class="roomname">Roomname: ' + _.escape(current.roomname) + '</span>';
+      var username = '<span class="username">Username: ' + _.escape(current.username) + '</span>';
+      var text = '<p class="text">Text: ' + _.escape(current.text) + '</p>';
 
-    console.log(start + roomname + text + username + end);
+      $('.messages').append(start + username + roomname + text + end);
+    }
+  } else {
+    var index = array.lastIndexOf();
+    for (var k = 0; k < array.length; k++) {
+      if (array[k].objectId === lastMessageId) {
+        index = k -1;
+        break;
+      }
+    }
+    for( var l = index; l >= 0; l-- ){
+      var current = array[l];
+      var roomname = '<span class="roomname">Roomname: ' + _.escape(current.roomname) + '</span>';
+      var username = '<span class="username">Username: ' + _.escape(current.username) + '</span>';
+      var text = '<p class="text">Text: ' + _.escape(current.text) + '</p>';
+
+      $('.messages').prepend(start + username + roomname + text + end);
+    }
   }
+  
+  lastMessageId = array[0].objectId;
+  console.log(lastMessageId);
 };
+
+setInterval(app.fetch, 1000);
+
+$(document).ready(function(){
+  app.fetch();
+});
+
+
+
+
+
+
+// Text: ..........................................................̣''҃҄҈҅҈҆҈҅҈҄҈҃҈҄҈҅҈҆҈҅҈҄҈҃҈҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҄҃҄҅҆҅҈҄҈҃҈҄҈҅҈҆҈҅҈҄҈҃҈҄҅҆҅҄҃҄
